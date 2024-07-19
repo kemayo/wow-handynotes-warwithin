016f391570d97fa6c4de8803ac7a75d08e5c4bb6
David Lynch <kemayo@gmail.com>
2024-07-19 14:09:34 -0500

    Initial commit

---
+++ warwithin.lua
@@ -0,0 +1,39 @@
+local myname, ns = ...
+
+ns.KHAZALGAR = 2274
+ns.DORNOGAL = 2339
+ns.ISLEOFDORN = 2248
+ns.RINGINGDEEPS = 2214
+ns.HALLOWFALL = 2215
+ns.AZJKAHET = 2255
+
+-- ns.MAXLEVEL = {ns.conditions.QuestComplete(67030), ns.conditions.Level(70)}
+-- ns.DRAGONRIDING = ns.conditions.SpellKnown(376777)
+
+--[[
+ns.PROF_WW_ALCHEMY =  -- spell:
+ns.PROF_WW_BLACKSMITHING =  -- spell:
+ns.PROF_WW_COOKING =
+ns.PROF_WW_ENCHANTING =  -- spell:
+ns.PROF_WW_ENGINEERING =  -- spell:
+ns.PROF_WW_FISHING =
+ns.PROF_WW_HERBALISM =
+ns.PROF_WW_INSCRIPTION =  -- spell:
+ns.PROF_WW_JEWELCRAFTING =  -- spell:
+ns.PROF_WW_LEATHERWORKING =  -- spell:
+ns.PROF_WW_MINING =
+ns.PROF_WW_SKINNING =
+ns.PROF_WW_TAILORING =  -- spell:
+--]]
+
+ns.hiddenConfig = {}
+
+ns.defaults.profile.groupsHidden = {}
+
+ns.groups["junk"] = BAG_FILTER_JUNK
+ns.groups["professionknowledge"] = "Profession Knowledge"
+
+--[[
+notes:
+
+--]]
