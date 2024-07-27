1c85c41a4c9f2303b2a57e33d7004692fdf5678e
David Lynch <kemayo@gmail.com>
2024-07-26 23:22:46 -0500

    Azj-Kahet adventurer achievement, quests were in the achievement

--- zones/AzjKahet.lua
+++ zones/AzjKahet.lua
@@ -2,32 +2,41 @@ local myname, ns = ...
 
 ns.RegisterPoints(ns.AZJKAHET, {
     [62204560] = { -- Kaheti Silk Hauler
-        -- [62204560, 62404140, 68205360]
-        quest=nil,
+        -- [62404140, 68205360]
+        criteria=69659,
+        quest=81702,
         npc=221327,
+        vignette=6134,
     },
     [75406140] = { -- XT-Minecrusher 8700
         -- [75406140, 75606120]
-        quest=nil,
+        criteria=69660,
+        quest=81703,
         npc=216034,
+        vignette=6131,
     },
     [47204320] = { -- Abyssal Devourer
         -- [47204320, 47204380]
-        quest=nil,
+        criteria=69651,
+        quest=81695,
         npc=216031,
         loot={
             223390, -- Leggings of Dark Hunger
             223391, -- Legguards of Dark Hunger
         },
+        vignette=6129,
     },
     [61207540] = { -- Maddened Siegebomber
         -- [61207540, 61407620, 61807500]
-        quest=nil,
+        criteria=69663,
+        quest=81706,
         npc=216044,
+        vignette=6138,
     },
     [36004480] = { -- Vilewing
         -- [36004480, 36204400, 36404580, 36604660, 36804320, 36804580, 37004540]
-        quest=nil,
+        criteria=69656,
+        quest=81700,
         npc=216037,
         loot={
             223386, -- Vilewing Crown
@@ -35,82 +44,121 @@ ns.RegisterPoints(ns.AZJKAHET, {
             223388, -- Vilewing Cap
             223405, -- Vilewing Visor
         },
+        vignette=6132,
     },
     [61203300] = { -- Webspeaker Grik'ik
-        quest=nil,
+        criteria=69655,
+        quest=81699,
         npc=216041,
+        vignette=6135,
     },
     [69802780] = { -- Cha'tak
-        quest=nil,
+        criteria=69661,
+        quest=81704,
         npc=216042,
+        vignette=6136,
     },
     [58206500] = { -- Enduring Gutterface
         -- [58206500, 58406560]
-        quest=nil,
+        criteria=69664,
+        quest=81707,
         npc=216045,
+        vignette=6139,
     },
     [68807200] = { -- Monstrous Lasharoth
-        quest=nil,
+        criteria=69662,
+        quest=81705,
         npc=216043,
+        vignette=6137,
     },
     [44803880] = { -- Rhak'ik
         -- [44803880, 44803980, 45204440]
-        quest=nil,
+        criteria=69653,
+        quest=81694,
         npc=221032,
+        -- vignette=,
     },
-    [64805540] = { -- Soulboil
-        -- [64805540, 64805560]
-        quest=nil,
-        npc=224282,
-    },
-    [40004740] = { -- Ahg'zagall
-        quest=nil,
-        npc=214151,
-    },
-    [65605580] = { -- Azerite Manifestation
-        quest=nil,
-        npc=222670,
-    },
+    --[[ -- with Rhak'ik:
     [44803980] = { -- Khak'ik
         -- [44803980, 45003780, 45403660]
-        quest=nil,
+        criteria=69653,
+        quest=81694,
         npc=216032,
+        vignette=,
     },
+    --]]
+    [40004740] = { -- Ahg'zagall
+        criteria=69654,
+        quest=78905,
+        npc=214151,
+        vignette=5973,
+    },
+    -- UNKNOWN LOCATION
+    [85008500] = { -- Umbraclaw Matra
+        criteria=69668,
+        quest=82037,
+        npc=216051,
+        vignette=6186,
+        note="UNKNOWN LOCATION",
+    },
+    [87008500] = { -- Deepcrawler Tx'kesh
+        criteria=69669,
+        quest=82077,
+        npc=222624,
+        vignette=6203,
+        note="UNKNOWN LOCATION",
+    },
+    --[[
+    [89008500] = { -- Kaheti Bladeguard
+        criteria=69670,
+        quest=82078,
+        npc=,
+        vignette=6204,
+        note="UNKNOWN LOCATION",
+    },
+    --]]
+}, {
+    achievement=40840, -- Adventurer
 })
 
 ns.RegisterPoints(2256, { -- Azj-Kahet Lower
     [64408720] = { -- Harvester Qixt
         -- [64408720, 64608520]
-        quest=nil,
+        criteria=69667,
+        quest=82036,
         npc=216050,
+        vignette=6185,
     },
     [64408840] = { -- The Oozekhan
-        quest=nil,
+        criteria=69666,
+        quest=82035,
         npc=216049,
+        vignette=6184,
     },
     [64608460] = { -- Jix'ak the Crazed
         -- [64608460, 65008580]
-        quest=nil,
+        criteria=69665,
+        quest=82034,
         npc=216048,
+        vignette=6183,
     },
+}, {
+    achievement=40840, -- Adventurer
 })
 
 ns.RegisterPoints(2213, { -- City of Threads
-    [69406220] = { -- Xishorr
-        quest=nil,
-        npc=216039,
-    },
     [36404160] = { -- The Groundskeeper
-        -- [36404160, 37204260, 38004340]
-        quest=nil,
+        criteria=69657,
+        quest=81634,
         npc=216038,
+        vignette=6111,
     },
-    [22204460] = { -- Vin'ris The Corruptor
-        quest=nil,
-        npc=222630,
+    [69406220] = { -- Xishorr
+        criteria=69658,
+        quest=81701,
+        npc=216039,
+        vignette=6133,
     },
-    [19004640] = { -- Yoh'nath The Ender
-        quest=nil,
-        npc=222628,
-    },
+}, {
+    achievement=40840, -- Adventurer
 })
