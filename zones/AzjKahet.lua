fb79c7f9eb4379fab5241a580819fe4e58b641f1
David Lynch <kemayo@gmail.com>
2024-08-07 23:48:05 -0500

    Azj-Kahet: Route for Kaheti Silk Hauler, start on Itsy Bitsy Spider

--- zones/AzjKahet.lua
+++ zones/AzjKahet.lua
@@ -85,15 +85,32 @@ ns.RegisterPoints(ns.CITYOFTHREADS, {
     parent=true,
 })
 
+-- Itsy Bitsy Spider
+ns.RegisterPoints(ns.AZJKAHET, {
+    -- [] = {criteria=68972, quest=nil}, -- Webster
+    -- [] = {criteria=68973, quest=nil}, -- Spindle
+    -- [] = {criteria=68974, quest=nil}, -- Swift
+    -- [] = {criteria=68976, quest=nil}, -- Ru'murh
+    -- [] = {criteria=68977, quest=nil}, -- Thimble
+    -- [] = {criteria=68978, quest=nil}, -- Scampering Weave-Rat
+    -- [] = {criteria=68979, quest=nil}, -- General's Scouting Shadecaster
+}, {
+    achievement=40624,
+    atlas="WildBattlePet", color={r=0.75, g=1, b=0},
+    minimap=true,
+})
+
 -- Rares
 
 ns.RegisterPoints(ns.AZJKAHET, {
-    [62492877] = { -- Kaheti Silk Hauler
+    [65201896] = { -- Kaheti Silk Hauler
         -- [62404140, 68205360]
         criteria=69659,
         quest=81702,
         npc=221327,
         vignette=6134,
+        route={65201896, 65142033, 63122532, 62492877, 61882919},
+        note="Slowly wanders back and forth",
     },
     [76585780] = { -- XT-Minecrusher 8700
         criteria=69660,
