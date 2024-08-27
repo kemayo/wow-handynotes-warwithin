488970e60379bf4fe0b6bd58e2985546ac620d55
David Lynch <kemayo@gmail.com>
2024-08-27 09:58:36 -0500

    Dornogal: add Flat Earthen achievement

--- zones/Dornogal.lua
+++ zones/Dornogal.lua
@@ -1,4 +1,10 @@
 local myname, ns = ...
 
 ns.RegisterPoints(ns.DORNOGAL, {
+    [50086133] = {
+        achievement=40606, -- Flat Earthen
+        note="Let yourself get crushed. To death.",
+        texture=ns.atlas_texture("vehicle-hammergold", {r=0, g=1, b=1, scale=1.3}),
+        minimap=true,
+    },
 })
