6af2ba99c9e349a28054a85831ac08e9714b85e0
David Lynch <kemayo@gmail.com>
2024-08-28 23:14:13 -0500

    Dornogal: Tome of Polymorph: Mosswool

--- zones/Dornogal.lua
+++ zones/Dornogal.lua
@@ -7,4 +7,12 @@ ns.RegisterPoints(ns.DORNOGAL, {
         texture=ns.atlas_texture("vehicle-hammergold", {r=0, g=1, b=1, scale=1.3}),
         minimap=true,
     },
+    [61230552] = { -- Tome of Polymorph: Mosswool
+        quest=84438, -- Easy Win
+        loot={{227710, class="MAGE"}},
+        path={61230552, 61910553, 62460535, 62860471, 61540265, 61100293, 58810639},
+        note="Talk to {npc:229128:Valeera Sanguinar} for the book",
+        atlas="class",
+        requires=ns.conditions.Class("MAGE"),
+    },
 })
