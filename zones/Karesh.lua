d4e49ab39fb29a2d883b1cbf63ccbf701d6bec5f
David Lynch <kemayo@gmail.com>
2025-08-03 16:47:49 -0500

    Stub of K'aresh

---
+++ zones/Karesh.lua
@@ -0,0 +1,23 @@
+local myname, ns = ...
+
+--[[
+notes:
+--]]
+
+-- Treasures
+
+ns.RegisterPoints(ns.KARESH, {
+}, {
+	achievement = 42741, -- Treasures of K'aresh
+})
+
+-- Things to find:
+-- Dangerous Prowlers of K'aresh (42729)
+-- Secrets of the K'areshi (60890)
+
+-- Rares
+
+ns.RegisterPoints(ns.KARESH, {
+}, {
+	achievement = 42761, -- Remnants of a Shattered World
+})
